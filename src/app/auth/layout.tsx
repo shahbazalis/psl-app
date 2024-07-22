16acd1274fb9abb5df9bdd41da958d6b6f4ea435
shahbazalis <engr.shahbaz92@gmail.com>
2024-07-22 22:34:58 +0300

    image added to the landing page and login page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,15 @@
-import React from "react";
 import Navbar from "@/components/Navbar";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-full">
-      <div className="flex flex-col w-full">
+    <section className="h-screen flex ">
+      <div
+        className="w-1/2 bg-cover bg-center"
+        style={{ backgroundImage: 'url(/images/psl.png)' }}
+      />
+      <div className="w-1/2 flex flex-col ">
         <Navbar />
-        <div className="h-screen flex items-center justify-center overflow-auto">
-          {children}
-        </div>
+        <div className="h-screen flex items-center justify-center overflow-auto">{children}</div>
       </div>
     </section>
   );
