22e0134567e215180063ba884a20f040d4f833f9
shahbazalis <engr.shahbaz92@gmail.com>
2024-08-07 15:14:06 +0300

    number of teams shown in a row changed in home component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -37,23 +37,18 @@ export default function Home() {
   if (loading) {
     return <LoadingComponent />;
   }
-const teamName = teams[0].name.replace(/\s+/g, '');
-console.log('Team Name:',teamName);
+
   return (
     <div className="w-full h-screen overflow-auto">
       <PageTitle title="Home" />
       <Image
-        //className="w-full h-64 sm:h-80 md:h-96 lg:h-[28rem] xl:h-[32rem] bg-cover bg-center"
-        //className="w-full h-48 sm:h-64 md:h-72 lg:h-[24rem] xl:h-[28rem] object-cover"
-        //className="h-1/3 object-cover"
         src="/images/banner.png"
         alt="banner"
         layout="responsive"
         width={1280}
         height={720}
       />
-
-      <section className="grid w-full grid-cols-1 gap-4 mt-4 mb-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <section className="grid w-full grid-cols-1 gap-4 mt-4 mb-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {teams.map((team) => (
           <div key={team.id} className="overflow-auto">
             <Card
@@ -64,14 +59,15 @@ console.log('Team Name:',teamName);
                 <CardTitle className="flex ">
                   <Image
                     className="w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32 rounded-full border-4 border-white shadow-lg mr-4 sm:mr-6 md:mr-8"
-                    src={`https://psl-s3-vercel-bucket.s3.us-east-2.amazonaws.com/${team.name.replace(/\s+/g, '')}.jpg`}
+                    src={`https://psl-s3-vercel-bucket.s3.us-east-2.amazonaws.com/${team.name.replace(
+                      /\s+/g,
+                      ""
+                    )}.jpg`}
                     alt={team.name}
                     width={128}
                     height={128}
                   />
-                  <span
-                    className="flex items-center justify-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl "
-                  >
+                  <span className="flex items-center justify-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl break-words leading-tight text-center">
                     {team.name}
                   </span>
                 </CardTitle>
